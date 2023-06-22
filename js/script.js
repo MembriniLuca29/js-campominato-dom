a63e46a2fb26021e50ce564b5054040a0851126f
MembriniLuca29 <lucamembrini29@gmail.com>
2023-06-22 17:51:43 +0200

    bonus completato

--- js/script.js
+++ js/script.js
@@ -16,28 +16,16 @@ function randomNumber(min, max) {
 
 generate.addEventListener("click",
 function tabella() {
-
     let clickCount = 0;
-
     termine.style.display = "none";
-    
     let randomNumbers = [];
-
 if (difficult.value == "facile"){
-
-
 while (randomNumbers.length < 16) {
     const aNumber = randomNumber(1, 100);
-    console.log(aNumber, typeof aNumber);
-
     if (!randomNumbers.includes(aNumber)) {
         randomNumbers.push(aNumber);
     }
-
-    
 }
-
-console.log(randomNumbers)
     const gamecontainer = document.getElementById("container");
     gamecontainer.innerHTML = "";
     for (let numero = 1; numero <= 100; numero++) {
@@ -47,30 +35,19 @@ console.log(randomNumbers)
         container.append(celle);
         celle.classList.add("celle")
 
-   
         celle.addEventListener('click', function () {
-            console.log(this.innerHTML);
-
             if (!this.classList.contains("clicked")) {
                 this.classList.add("clicked");
                 clickCount++;
                 console.log("Casella " + this.textContent + " cliccata" , clickCount);
         };
-
-          
         let numeroSelezionato = parseInt(this.innerHTML) ;
-        
-
-            
-            for (var i = 0; i < randomNumbers.length; i++) {
+            for (let i = 0; i < randomNumbers.length; i++) {
                 if (numeroSelezionato === randomNumbers[i]) {
                   console.log("La variabile è uguale a uno dei valori dell'array.");
                   break; 
                 }
               }
-
-              console.log("mio", numeroSelezionato , randomNumbers[i])
-
         if(numeroSelezionato === randomNumbers[i]){
             celle.classList.add("bomb");
             termine.style.display = "flex";
@@ -83,25 +60,18 @@ console.log(randomNumbers)
         else if( numeroSelezionato != randomNumbers[i]) {
             celle.classList.add("active");
         }
-            
     },{once : true}
     )
     }
 }
-
 else if (difficult.value == "normale"){
-
 while (randomNumbers.length < 16) {
     const aNumber = randomNumber(1, 81);
-    console.log(aNumber, typeof aNumber);
 
     if (!randomNumbers.includes(aNumber)) {
         randomNumbers.push(aNumber);
     }
-
-    
 }
-    console.log(randomNumbers)
     const gamecontainer = document.getElementById("container");
     gamecontainer.innerHTML = "";
     for (let numero = 1; numero <= 81; numero++) {
@@ -111,36 +81,22 @@ while (randomNumbers.length < 16) {
         container.append(celle);
         celle.classList.add("celle")
         celle.classList.add("normal")
-     
-        
         celle.addEventListener('click', function () {
-            console.log(this.innerHTML);
-
             if (!this.classList.contains("clicked")) {
                 this.classList.add("clicked");
                 clickCount++;
-                console.log("Casella " + this.textContent + " cliccata" , clickCount);
         }
-
-          
         let numeroSelezionato = parseInt(this.innerHTML) 
-        
-
-            
             for (var i = 0; i < randomNumbers.length; i++) {
                 if (numeroSelezionato === randomNumbers[i]) {
                   console.log("La variabile è uguale a uno dei valori dell'array.");
                   break; 
                 }
               }
-
-              console.log("mio", numeroSelezionato , randomNumbers[i])
-
         if(numeroSelezionato === randomNumbers[i]){
             celle.classList.add("bomb");
             termine.style.display = "flex";
             document.getElementById("punteggio").innerHTML = "hai cliccato correttamente: " + (clickCount - 1) + " celle";
-
         }
         else if (clickCount == 65) {
             termine.style.display = "flex";
@@ -149,28 +105,19 @@ while (randomNumbers.length < 16) {
         else if( numeroSelezionato != randomNumbers[i]) {
             celle.classList.add("active");
         }
-           
-        
-        
 },{once : true}
     )
     }
 }
 else if (difficult.value == "difficile"){
 
-console.log("Casella " + this.textContent + " cliccata" , clickCount);
-
 while (randomNumbers.length < 16) {
     const aNumber = randomNumber(1, 49);
-    console.log(aNumber, typeof aNumber);
 
     if (!randomNumbers.includes(aNumber)) {
         randomNumbers.push(aNumber);
     }
-
-    
 }
-console.log(randomNumbers)
     const gamecontainer = document.getElementById("container");
     gamecontainer.innerHTML = "";
     for (let numero = 1; numero <= 49; numero++) {
@@ -181,29 +128,19 @@ console.log(randomNumbers)
         celle.classList.add("celle")
         celle.classList.add("hard")
        
-    
-     
         celle.addEventListener('click', function () {
-            console.log(this.innerHTML);
-
             if (!this.classList.contains("clicked")) {
                 this.classList.add("clicked");
                 clickCount++;
-                console.log("Casella " + this.textContent + " cliccata" , clickCount);
         }
-
         let numeroSelezionato = parseInt(this.innerHTML) 
-        
-            
+    
             for (var i = 0; i < randomNumbers.length; i++) {
                 if (numeroSelezionato === randomNumbers[i]) {
                   console.log("La variabile è uguale a uno dei valori dell'array.");
                   break; 
                 }
               }
-
-              console.log("mio", numeroSelezionato , randomNumbers[i])
-              console.log(clickCount,"allll")
 
         if(numeroSelezionato === randomNumbers[i]){
             celle.classList.add("bomb");
@@ -214,25 +151,12 @@ console.log(randomNumbers)
             termine.style.display = "flex";
             document.getElementById("punteggio").innerHTML = "hai vinto";
         }
-
         else if( numeroSelezionato != randomNumbers[i]) {
             celle.classList.add("active");
         }
-      
-        
-
-           
-        
-        
 },{once : true}
     )
     }
 }   
-}
- ,
+} ,
 )
-
-
-// {once : true}
-
-
